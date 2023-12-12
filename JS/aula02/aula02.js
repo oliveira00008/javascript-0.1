98e1eada113b3addf6baa10482b7a3b73c92b1c1
oliveira00008 <gabrielgithub00008@gmail.com>
2023-12-12 05:31:58 -0300

    atualisando meu js.01

---
+++ JS/aula02/aula02.js
@@ -0,0 +1,39 @@
+document.write("ola mundo")
+console.log("ola outra ves mundo")
+
+let mensagem = " minha mensagem"
+
+let mensagem2 = " outra mensagem"
+
+//posso usar uma variavel camelKeise que seria minuscula e maiuscula ex =.> meuPeso
+
+let meuPeso = 75.6
+
+let minhaAltura = 1.75
+
+let minhaIdade = 25
+
+//nessa variavel siguinifica false ou tru se chama valor boleano.
+let ehDoador = true
+
+let texte = null
+
+/*CRIAR CONSTANTE e atribuir valor*/
+
+const PI = 3.1415
+
+const TAXA = 0.3 
+
+texte = "algun coteudo"
+
+/* ESCREVER CONTEUDO DE VARIAVEL E CONTANTE*///e para concatenar nos usamos  aspa ("") e cplocamos paragrafo <p> tambem psso usar um termo de templeid estring (``) */ 
+
+document.write("<p> mensagem" + mensagem + "</p>")
+document.write(`mensagem2: ${mensagem2}`)
+document.write(`meuPeso: ${meuPeso} kg <p><br></p>`)
+document.write(`minhaAltura: ${minhaAltura} <p><br></p>`)
+document.write(`minhaIdade: ${minhaIdade} <p><br></p> `)
+document.write(`ehDoador: ${ehDoador} <p><br></p>`)
+document.write(`texte: ${texte} <p><br></p>`)
+document.write(`PI: ${PI} <p><br></p>`)
+document.write(`TAXA: ${TAXA} <p><br></p> `)
