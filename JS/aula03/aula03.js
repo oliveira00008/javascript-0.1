bcee107cd3e1fe347b70f5ea45903831d1b8e598
oliveira00008 <gabrielgithub00008@gmail.com>
2023-12-13 00:22:21 -0300

    atualisando meu exercicio aula 3 js

---
+++ JS/aula03/aula03.js
@@ -0,0 +1,35 @@
+/*Aquivos js aiala 03 operadores dee Operaçoes*/
+
+let n1 = 10 //number inteiro 
+let n2 = 2 //nuber inteiro
+let n3 = "2"//string texto
+
+document.write(`n1 ${n1} <br>`)
+document.write(`n2 ${n2} <br>`)
+document.write(`n3 ${n3} <br>`)
+
+console.log(n1 + n2)//adiçao
+console.log(n1 - n2)//subtraçao
+console.log(n1 * n2)//multiplicaçao
+console.log(n1 / n2)//divisao
+console.log(n1 % n2)//resto ou modulo
+
+console.log(n2 == n3)//igual a 
+console.log(n2 === n3)//identico a
+console.log(n2 != n3)//diferente ne nao =
+console.log(n2 !== n3)//nao e identico
+
+console.log(n1 > n2)//maior que 
+console.log(n1 < n2)//menor que 
+console.log(n1 >= n2)//maior igual a
+console.log(n1 <= n2)//menor igual a 
+
+//operadores logico relacinais boleano
+
+let a = true
+let b = false
+
+console.log( a && b)//E AND
+console.log(a || b)//OU OR
+console.log(!a)//NAO NOT negaçao
+console.log(!b)
