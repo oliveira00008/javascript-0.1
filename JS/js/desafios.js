c763d741b3c09fe75bec6057c28d3730edc4cce1
oliveira00008 <gabrielgithub00008@gmail.com>
2023-12-16 04:19:48 -0300

    atualisando meu exercicios

---
+++ JS/js/desafios.js
@@ -0,0 +1,22 @@
+//desafio 01 
+let nome = 'Rorion Gracie'
+let Nacionalidade = 'Brasil'
+let idade = 44
+let peso = '106. kg'
+let altura = '1,85 (m)'
+
+/////////////////////////////////////
+
+let frase ='A amizade desenvolve a felicidade e reduz o sofrimento, duplicando a nossa alegria e dividindo a nossa dor.'
+let data1 = '10/9/2021'
+let autor = 'Aeron Swartz'
+
+document.write(`
+     <div>
+    <h1>Frase do dia</h1>
+    <p>${data1}<p>
+    <p>${frase}</p>
+    <p>${autor}</p>
+    </div>
+`)
+
